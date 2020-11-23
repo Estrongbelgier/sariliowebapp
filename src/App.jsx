855f28f2f5a58ca2ebb4e7bc68f817c091593473
Jose Ivan Ribeiro de Oliveira <ivan_r.o@hotmail.com>
2020-11-23 20:14:28 -0300

    dev: fixing router.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import './config/ReactotronConfig';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Router, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Routes from '~/routes/index.jsx';
-
-import history from '~/services/history';
 
 import 'react-toastify/dist/ReactToastify.css';
 import '~/assets/styles/global.css';
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import history from '~/services/history';
 import Profile from '~/pages/Profile/index.jsx';
 import Book from '~/pages/Book/index.jsx';
 import Home from '~/pages/Home/index.jsx';
@@ -11,14 +11,14 @@ import MeusAtivos from '~/pages/MeusAtivos/index.jsx';
 
 export default function Routes() {
   return (
-    <Switch>
+    <Switch history={history}>
       <Route path="/" exact component={Home} />
       <Route path="/login" component={Login} />
+      <Route path="/app" component={AppPage} />
       <Route path="/register" component={Register} />
       <Route path="/book" component={Book} />
       <Route path="/meusativos" component={MeusAtivos} />
       <Route path="/profile" component={Profile} />
-      <Route path="/app" component={AppPage} />
       <Route />
     </Switch>
   );
